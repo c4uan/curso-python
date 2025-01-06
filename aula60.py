7bfd3b95d8ce3fbad15d9f7c07413a234ee07be5
Cauan <cauanpjcts@gmail.com>
2025-01-06 16:07:06 -0300

    Operações ternárias

---
+++ aula60.py
@@ -0,0 +1,16 @@
+"""
+Operação ternária (condicional de uma linha)
+<expressão1> if <condição> else <expressão2>
+"""
+
+# condicao = 10 < 5
+# variavel = 'sim' if condicao else 'não'
+
+# print(variavel)
+
+# digito = 4
+# outro_digito = digito if digito <=9 else 0
+# novo_digito = 0 if digito > 9 else digito
+# print(outro_digito, novo_digito)
+
+print('Valor' if False else 'Outro Valor' if False else 'Fim')
