8c26ba4bd6cc45ea9c1d36a709c3a4a168dca8e7
Cauan <cauanpjcts@gmail.com>
2025-01-06 17:14:22 -0300

    Cálculo do primeiro dígito do CPF

--- aula61.py
+++ aula61.py
@@ -13,21 +13,3 @@ for digit in nove_digitos:
 
 digito_1 = (resultado_digito_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
-
-dez_digitos = nove_digitos + str(digito_1)
-contr_2 = 11
-
-resultado_digito_2 = 0
-for digit in dez_digitos:
-    resultado_digito_2 += int(digit) * contr_2
-    contr_2 -= 1
-
-digito_2 = (resultado_digito_2 * 10) % 11
-digito_2 = digito_2 if digito_2 <=9 else 0
-
-cpf_gerado = f'{nove_digitos}{digito_1}{digito_2}'
-
-if cpf_gerado == entrada_cpf:
-    print('CPF válido')
-else:
-    print('CPF inválido')
