3b4ee639151fd47e1e9213fe342f45b6085b9746
Cauan <cauanpjcts@gmail.com>
2025-01-07 15:19:38 -0300

    Introdução às funções

---
+++ aula65.py
@@ -0,0 +1,19 @@
+"""
+introdução a funções
+trechos de código que podem ser reutilizados
+Elas podem receber parâmetros (argumentos)
+e retornar um valor específico
+Por padrão, funções retornam None.
+"""
+
+# def imprimir(a, b, c):
+#     print('Oi')
+
+# imprimir(1, 2, 3)
+
+def saudacao(nome):
+    print(f'Olá, {nome}')
+
+saudacao('João')
+saudacao('Maria')
+saudacao('José')
