9087908f1381303ae86b6934a0cac2f4b8066b0c
Cauan <cauanpjcts@gmail.com>
2025-01-14 10:11:04 -0300

    Métodos úteis dos dictionaries em Python

--- aula78.py
+++ aula78.py
@@ -4,6 +4,10 @@
 # values - iterável com os valores
 # items - iterável com chaves e valores
 # setdefault - adiciona valor se a chave não existe
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
 
 pessoa = {
     'nome': 'Cauan',
